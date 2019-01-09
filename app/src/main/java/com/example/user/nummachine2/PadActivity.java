3ccc2bf90dea45b2e0f8307e2be93500fb4f964c
Steven Liao <steven.liao@mitake.com.tw>
2019-01-09 09:52:05 +0800

    01/09 公司上傳

--- app/src/main/java/com/example/user/nummachine2/PadActivity.java
+++ app/src/main/java/com/example/user/nummachine2/PadActivity.java
@@ -1,8 +1,6 @@
 package com.example.user.nummachine2;
 
-import android.app.FragmentManager;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -10,15 +8,8 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
-
-import java.util.EnumMap;
-import java.util.Map;
 
 public class PadActivity extends AppCompatActivity {
 
@@ -36,7 +27,9 @@ public class PadActivity extends AppCompatActivity {
         SocketTool.initSocketTool();
 
         //建立一個新的Socket並以每兩秒發送號碼查詢
-        SocketTool.createAndSendSocket(storeName, socketHandler, PadActivity.this);
+        SocketTool.createSocket();
+
+        SocketTool.sendSocket(storeName, socketHandler, PadActivity.this);
 
     }
 
@@ -60,7 +53,7 @@ public class PadActivity extends AppCompatActivity {
                 case 0:
                     BarcodeEncoder encoder = new BarcodeEncoder();
                     try {
-                        
+
                         Bitmap bit = encoder.encodeBitmap(qrMsg, BarcodeFormat.QR_CODE, 250, 250);
                         ivCode.setImageBitmap(bit);
 
@@ -83,5 +76,9 @@ public class PadActivity extends AppCompatActivity {
         } else {
             return strArray[1];
         }
+    }
+
+    public interface createSocketCallback{
+        public void onSocketCallback();
     }
 }
